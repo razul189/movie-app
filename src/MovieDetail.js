af67530d73d2ee34d8a456cb3f0842f4fc08fb77
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-08-28 17:08:28 -0400

    MovieDetail component started

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,10 +1,31 @@
-import React, {useState, useEffect} from "react"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const MovieDetail = () => {
-    return (
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
 
-    )
-}
+  useEffect(() => {
+    fetch(`http://localhost:3000/movies/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovie(data)
+      });
+  }, [id])
 
-export default MovieDetail
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Title:{movie.title}</h1>
+      <h2>Directors:{movie.director}</h2>
+      <img src={movie.imgUrl} alt="whoops" />
+      <p>Synopsis:{movie.synopsis}</p>
+    </div>
+  );
+};
+
+export default MovieDetail;
 
