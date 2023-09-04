3f0e45d25e65dadf34e593e6c7c2964f0770169e
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-09-04 15:56:20 -0400

    About component added onto

--- src/About.js
+++ src/About.js
@@ -1,5 +1,25 @@
-import React from "react"
+import React from 'react'
 
+const About = () => {
+    return (
+        <div className="about-container">
+            <h1 className="about-title">About Me</h1>
+            <div className="divider"></div>
+            <p className="about-text">
+                I am a software engineering student at Flatiron School. I created this project using React, JSX, HTML, and CSS. 
+                I truly enjoyed working on this as it was fun exploring all the different things I can do with React such as 
+                component-based architecture, reusability, and the ability to manage states effortlessly.
+            </p>
+            
+            <h2 className="about-subtitle">filmFolio</h2>
+            <div className="divider"></div>
+            <p className="about-text">
+                This was created for movie lovers like myself to keep track of their favorite movies! Whether you're looking for 
+                action-packed blockbusters, thought-provoking films, or timeless classics, I've got you covered. Just go ahead and 
+                add them on! Thank you for checking out filmFolio!
+            </p>
+        </div>
+    );
+}
 
-
-export default About
+export default About;
